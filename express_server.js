96fa568350c5157ed913fa492c33c970847af63f
mikyYun <mkyun2714@gmail.com>
2022-04-19 19:16:07 -0700

    before making logout function

--- express_server.js
+++ express_server.js
@@ -2,6 +2,7 @@
 // It will then add the data to the request objcet under the key body
 // the data in the 'input' filed will be available to us in the request.body.longURL variable that we can store in 'urlDatabase' object
 const bodyParser = require('body-parser');
+const cookieParser = require('cookie-parser')
 // const { response } = require('express');
 
 const express = require('express');
@@ -10,6 +11,8 @@ const PORT = 8080;
 
 // app.set('view engine', 'ejs');
 app.set("view engine", "ejs");
+app.use(cookieParser());
+app.use(bodyParser.urlencoded({extended: true}));
 
 const urlDatabase = {
   // 'b2xVn2': 'http://www.lighthouselabs.ca',
@@ -36,23 +39,31 @@ app.get('/hello', (request, response) => {
 
 // new route handler for '/urls'. and pass the url data to our template by using .render()
 app.get('/urls', (request, response) => {
-  const templateVars = { urls: urlDatabase };
+  const templateVars = {
+    urls: urlDatabase,
+    username: request.cookies['username']
+  }
+  // console.log(request.cookies)
+  // console.log('Cookies: ', request.cookies)
   response.render('urls_index', templateVars);
 });
 
 // The order of route definition matters.
 // GET /urls/new route needs to be difined before the :id. Routes defined earlier will take precedence -> if we place this route after :id, any calls to /urls/new will be handled by app.get('/urls/:id', ...) because Express will think that 'new' is a route parameter
 app.get("/urls/new", (request, response) => {
-  response.render("urls_new");
+  const templateVars = {
+    urls: urlDatabase,
+    username: request.cookies['username']
+  }
+  response.render("urls_new", templateVars);// rendering
 });
 
 app.get("/urls/:shortURL", (request, response) => {
-  const templateVars = { shortURL: request.params.shortURL, longURL: urlDatabase[request.params.shortURL] };
+  const templateVars = { shortURL: request.params.shortURL, longURL: urlDatabase[request.params.shortURL], username: request.cookies['username'] };
   response.render("urls_show", templateVars);
   // response.render("./partials/urls_show_copy.html", templateVars);
 });
 
-app.use(bodyParser.urlencoded({extended: true}));
 
 function generateRandomString () {
   let randomString = Math.random().toString(32)
@@ -72,6 +83,7 @@ app.post('/urls', (request, response) => {
 })
 
 app.get("/u/:shortURL", (request, response) => {
+
   const longURL = urlDatabase[request.params.shortURL]
   if (!longURL) {
     response.sendStatus(404)
@@ -98,6 +110,7 @@ app.post('/urls/:shortURL/edit', (request, response) => {
 })
 
 app.post('/urls/:id', (request, response) => {
+  
   console.log("TEST", request.params.id)
   const shortURL = request.params.id
   // const edit
@@ -113,3 +126,27 @@ app.post('/urls/:id', (request, response) => {
   // urlDatabase[newShortURL] = newLongURL;
   response.redirect('/urls/')
 })
+
+app.post('/login', (request, response) => {
+  // console.log(request.body.username)
+  const newUserId = request.body.username
+  // response.cookie('username', newUserId)
+  console.log('username!!!!')
+  // console.log(response.cookie(newUserId))
+  response.redirect('/urls')
+})
+// app.get('/logout', (request, response) => {
+//   console.log("TEST", request.cookies)
+// })
+app.post('/logout', (request, response) => {
+  console.log(request.body.username)
+  // const newUserId = request.body.username
+  // response.cookie('username', newUserId)
+  // console.log('username!!!!')
+  // console.log(response.cookie(newUserId))
+  response.redirect('/urls')
+})
+// app.use(cookieParser());
+
+
+// /pkill node
